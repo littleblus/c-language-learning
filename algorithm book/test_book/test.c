caa8fb76f25c3b9a0d59f848a63fe7504ba46542
littleblus <gqstudio004@foxmail.com>
2022-12-21 21:04:38 +0800

    book test

---
+++ algorithm book/test_book/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define maxn 105
+
+#include <stdio.h>
+#include <string.h>
+
+int less(const char* s, int p, int q)
+{
+	int n = strlen(s);
+	printf("%d\n", n);
+
+	return 0;
+}
+
+int main()
+{
+	char s[maxn] = "abcdef";
+	less(s, 0, 0);
+
+	return 0;
+}
