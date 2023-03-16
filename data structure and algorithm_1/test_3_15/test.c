a5e604bdfd1a7ed93258d6eeead334c23e96f6a7
littleblus <gqstudio004@foxmail.com>
2023-03-16 18:09:51 +0800

    链表的深拷贝O(N)与栈

--- data structure and algorithm_1/test_3_15/test.c
+++ data structure and algorithm_1/test_3_15/test.c
@@ -4,76 +4,76 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//bool isValid(char* s) {
-//    struct stack {
-//        char* data;
-//        int top;
-//        int capacity;
-//    }Stack;
-//    Stack.data = (struct stack*)malloc(sizeof(char) * 100);
-//    Stack.capacity = 100;
-//    Stack.top = 0;
-//    char* cur = s;
-//    while (*cur) {
-//        if (Stack.top == Stack.capacity) {//判断容量
-//            Stack.data = (struct stack*)realloc(Stack.data, Stack.capacity * 2);
-//            Stack.capacity *= 2;
-//        }
-//        if (*cur == '(' || *cur == '[' || *cur == '{') {
-//            //入栈
-//            Stack.data[Stack.top] = *cur;
-//            Stack.top++;
-//        }
-//        else {
-//            //判断栈顶
-//            if (Stack.top == 0) {//栈为空并且不是左括号
-//                free(Stack.data);
-//                return false;
-//            }
-//            switch (*cur) {
-//            case ')':
-//                if ('(' == Stack.data[Stack.top - 1]) {//相等则出栈
-//                    Stack.top--;
-//                }
-//                else {
-//                    free(Stack.data);
-//                    return false;
-//                }
-//                break;
-//            case ']':
-//                if ('[' == Stack.data[Stack.top - 1]) {//相等则出栈
-//                    Stack.top--;
-//                }
-//                else {
-//                    free(Stack.data);
-//                    return false;
-//                }
-//                break;
-//            case '}':
-//                if ('{' == Stack.data[Stack.top - 1]) {//相等则出栈
-//                    Stack.top--;
-//                }
-//                else {
-//                    free(Stack.data);
-//                    return false;
-//                }
-//                break;
-//            default:
-//                free(Stack.data);
-//                return false;
-//                break;
-//            }
-//        }
-//        cur++;
-//    }
-//    free(Stack.data);
-//    if (Stack.top == 0) {
-//        return true;
-//    }
-//    else {
-//        return false;
-//    }
-//}
+bool isValid(char* s) {
+    struct stack {
+        char* data;
+        int top;
+        int capacity;
+    }Stack;
+    Stack.data = (struct stack*)malloc(sizeof(char) * 100);
+    Stack.capacity = 100;
+    Stack.top = 0;
+    char* cur = s;
+    while (*cur) {
+        if (Stack.top == Stack.capacity) {//判断容量
+            Stack.data = (struct stack*)realloc(Stack.data, Stack.capacity * 2);
+            Stack.capacity *= 2;
+        }
+        if (*cur == '(' || *cur == '[' || *cur == '{') {
+            //入栈
+            Stack.data[Stack.top] = *cur;
+            Stack.top++;
+        }
+        else {
+            //判断栈顶
+            if (Stack.top == 0) {//栈为空并且不是左括号
+                free(Stack.data);
+                return false;
+            }
+            switch (*cur) {
+            case ')':
+                if ('(' == Stack.data[Stack.top - 1]) {//相等则出栈
+                    Stack.top--;
+                }
+                else {
+                    free(Stack.data);
+                    return false;
+                }
+                break;
+            case ']':
+                if ('[' == Stack.data[Stack.top - 1]) {//相等则出栈
+                    Stack.top--;
+                }
+                else {
+                    free(Stack.data);
+                    return false;
+                }
+                break;
+            case '}':
+                if ('{' == Stack.data[Stack.top - 1]) {//相等则出栈
+                    Stack.top--;
+                }
+                else {
+                    free(Stack.data);
+                    return false;
+                }
+                break;
+            default:
+                free(Stack.data);
+                return false;
+                break;
+            }
+        }
+        cur++;
+    }
+    free(Stack.data);
+    if (Stack.top == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 struct Node {
     int val;
